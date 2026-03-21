73dd3f4a094ff599588f4d2d4d0db24a34d4677d
difficy <85756767+difficy@users.noreply.github.com>
2026-03-21 12:44:24 +0800

    Add files via upload

---
+++ leetcode-27.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        if (n == 0) {
+            return 0;
+        }
+        int* p = &nums[0];
+        int* q = &nums[n - 1];
+        while (p < q) {
+            while (p < q) {
+                if (*q != val) {
+                    break;
+                }
+                q--;
+                n--;
+            }
+
+            if (*p == val) {
+                n--;
+                *p = *q;
+                q--;
+            }
+            p++;
+        }
+        if (p == q && *p == val) {
+            n--;
+        }
+        return n;
+    }
+};
